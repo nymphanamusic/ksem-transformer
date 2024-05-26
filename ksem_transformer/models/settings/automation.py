a5fc8255606c013b027071445900af924a5333f1
Nymphana <hi@nymphana.com>
2024-05-25 21:51:52 -0400

    fix: :bug: fix arbitrary type not being allowed

--- ksem_transformer/models/settings/automation.py
+++ ksem_transformer/models/settings/automation.py
@@ -1,6 +1,6 @@
 from typing import Literal, get_args
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from ksem_transformer.models.ksem_json_types import KsemAutomationSettings
 from ksem_transformer.models.note_field import NoteField
@@ -10,6 +10,8 @@ AutomationKeyResets = Literal["only_this_track", "all_ksem_instances"]
 
 
 class Automation(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     automation_key: NoteField = Note.from_str("C7")
     automation_key_resets: AutomationKeyResets = "only_this_track"
     ignore_keyswitch_notes_in_midi_clips: bool = True
